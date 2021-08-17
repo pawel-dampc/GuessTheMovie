86a863fec80e005b50d720c9a8515a070306a316
Tomasz Bieńkowski <yobombel@gmail.com>
2021-08-17 15:36:24 +0200

    Several minor improvements in code.

--- src/Game.java
+++ src/Game.java
@@ -1,56 +1,73 @@
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Game {
 
     String movie;
-    int chances;
+
 
     //Constructor
     Game(String movie){
         this.movie = movie;
-        this.chances = 10;
     }
 
     public static void startGame(String movie) {
+        int chances = 10;
         Scanner scanner = new Scanner(System.in);
         String guess = "";
-        char[] codedMovie = movie.replaceAll("[a-z]","_").toCharArray();
         ArrayList<String> alreadyGuessed = new ArrayList();
+        char[] codedMovie = movie.replaceAll("[a-z]", "_").toCharArray();
+
+        //Greeting
+        System.out.println("Welcome to Guess the movie game.");
+        System.out.println("Pick a letter and try to reveal the title of the movie. You have 10 guesses.");
+        System.out.println("Good luck!");
+        System.out.println();
+        System.out.println("You're guessing: " + movie.replaceAll("[a-z]","_"));
+
+        //System.out.println("You're guessing: " + codedMovie); WTF THIS DOESN'T WORK??
 
         //CHEAT TO DELETE
         System.out.println(movie);
-        System.out.println("Type your guess: ");
-        guess = scanner.nextLine();
 
-        if(alreadyGuessed.contains(guess)){
-            System.out.println("You already guessed that character!");
-        }
-        else{
-            alreadyGuessed.add(guess);
-            if(movie.contains(guess)) {
-                //Checking for multiple instances of guessed character
-                ArrayList<Integer> indexes = new ArrayList<Integer>();
-               //Adds first instance of guessed index
-                int index = movie.indexOf(guess);
-                 //Adds later instances
-                while(index >= 0) {
-                     indexes.add(index);
-                     index = movie.indexOf(guess, index+1);
+        while (chances > 0) {
+            System.out.println("You have " + chances + " more guesses.");
+            System.out.println("Type your guess: ");
+
+            guess = scanner.nextLine();
+            guess = guess.toLowerCase();
+
+            //CHECK IF CHARACTER ALREADY GUESSED
+            if (alreadyGuessed.contains(guess)) {
+                System.out.println("You already guessed that character!");
+                System.out.println("List of already guessed chars: " + alreadyGuessed);
+            } else {
+                alreadyGuessed.add(guess);
+
+                //GOOD GUESS
+                if (movie.contains(guess)) {
+                    //MULTIPLE INSTANCES
+                    ArrayList<Integer> indexes = new ArrayList<Integer>();
+                    int index = movie.indexOf(guess);
+
+                    while (index >= 0) {
+                        indexes.add(index);
+                        index = movie.indexOf(guess, index + 1);
+                    }
+                    for (int i = 0; i < indexes.size(); i++) {
+                        codedMovie[indexes.get(i)] = guess.charAt(0);
+                    }
+                    System.out.println(codedMovie);
+                } else {
+                    System.out.println("Bad letter");
                 }
-                for(int i=0; i < indexes.size(); i++) {
-                codedMovie[indexes.get(i)] = guess.charAt(0);
-                }
-                System.out.println(codedMovie);
-            }
-             else{
-            System.out.println("Bad letter");
             }
         }
+    }
 
 
 
 
 
     }
-}
--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,7 @@ public class Main {
             String filePath = "movies.txt";
             String movie = Movie.randomMoviePicker(filePath);
 
-            //Greeting
-            System.out.println("Welcome to Guess the movie game.");
-            System.out.println("Pick a letter and try to reveal the title of the movie. You have 10 guesses.");
-            System.out.println("Good luck!");
 
-            //Move to game?
-            System.out.println("You're guessing: " + movie.replaceAll("[a-z]","_"));
 
             Game game = new Game(movie);
             Game.startGame(movie);
