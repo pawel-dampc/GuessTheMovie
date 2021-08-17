31830d36d663729c5be1229d7340dd56446aa00e
Tomasz Bieńkowski <yobombel@gmail.com>
2021-08-17 16:11:32 +0200

    Cleaning.

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,12 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
 
-        //Picks random movie
         try {
             String filePath = "movies.txt";
             String movie = Movie.randomMoviePicker(filePath);
 
-
-
             Game game = new Game(movie);
             Game.startGame(movie);
-
-
-            //Game.guess(movie);
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
 
         } catch (FileNotFoundException exception){
             System.out.println("File not found!");
